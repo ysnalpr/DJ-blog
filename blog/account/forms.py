147b1f6ecb5dd328553ca25a9b29edac82282558
ysnalpr <yasinalipour01@gmail.com>
2023-10-10 23:19:25 +0330

    User registration added

--- blog/account/forms.py
+++ blog/account/forms.py
@@ -1,6 +1,38 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        label="Password", widget=forms.PasswordInput, validators=[validate_password]
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput,
+        validators=[validate_password],
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "email"]
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd["password"] != cd["password2"]:
+            raise forms.ValidationError("Passwords don't match")
+        return cd["password2"]
+
+    def clean_email(self):
+        valid_domains = [
+            "gmail.com",
+            "yahoo.com",
+            "hotmail.com",
+            "live.com",
+            "outlook.com",
+        ]
+        cd = self.cleaned_data
+        domain = cd["email"].split("@")[-1]
+        if domain not in valid_domains:
+            raise forms.ValidationError("Enter a valid email")
+        return cd["email"]
--- blog/account/urls.py
+++ blog/account/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = "account"
 
 urlpatterns = [
-    # login / logout urls
+    # login / logout / register urls
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("register/", views.register, name="register"),
     # change password urls
     path(
         "password-change/",
